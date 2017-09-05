a539fd7f120b9bd6523c4e69e7f22dde9e8249c6
adam f <afiggat@gmail.com>
2017-09-04 19:16:25 -0600

    completed and passed the node assessment

--- server/server.js
+++ server/server.js
@@ -8,26 +8,26 @@ app.use(bodyParser.json());
 //1.
 app.get('/api/users', usersCtrl.getAll)
 
-//2.
-app.get('./api/users:id', usersCtrl.getAll)
+// //2.
+app.get('/api/users/:id', usersCtrl.getUserById)
 
-//3.
-app.get('./api/admins', usersCtrl.getAdmins)
+// //3.
+app.get('/api/admins', usersCtrl.getAdmins)
 
-//4.
-app.get('./api/nonadmins', usersCtrl.getNonAdmins)
+// //4.
+app.get('/api/nonadmins', usersCtrl.getNonAdmins)
 
-//5.
-app.get('./api/user_type/usertype:', userCtrl.getUsersByType)
+// //5.
+app.get('/api/user_type/:usertype', usersCtrl.getUsersByType)
 
-//.6
-app.put('./api/users/id:', usersCtrl.putUsersId)
+// //.6
+app.put('/api/users/:id', usersCtrl.putUsersId)
 
-//.7
-app.post('./api/users', usersCtrl.postUsers)
+// //.7
+app.post('/api/users', usersCtrl.postUsers)
 
-//8.
-app.delete('./api/users/id:', usersCtrl.deleteUsers)
+// //8.
+app.delete('/api/users/:id', usersCtrl.deleteUsers)
 
 const port = 3000;
 app.listen(port, () => { console.log(`Server be listening on port ${port}.`)});
--- server/usersCtrl.js
+++ server/usersCtrl.js
@@ -2,30 +2,139 @@ const userData = require('../userData.json');
 
 module.exports = {
 
-//1.
-getAll: (req, res)=> {
-    res.status(200).send(userData)
-} ,
+    // //1.
+    getAll: (req, res) => {
+        if (Object.keys(req.query).length == 0) {
+            res.status(200).send(userData)
+        }
+
+        let key = Object.keys(req.query);
+        // create an empty arr
+        let fav = []
+        let age = []
+        let lastname = []
+        let email = []
+
+        if (key[0] === 'lastname') {
+            userData.map(function (user) {
+                if (user.last_name == req.query.lastname) {
+                    lastname.push(user)
+                }
+            })
+            setTimeout(function () {
+                res.status(200).send(lastname)
+            }, 300)
+        }
+
+        if (key[0] === 'email') {
+            userData.map(function (user) {
+                if (user.email == req.query.email) {
+                    email.push(user)
+                }
+            })
+            setTimeout(function () {
+                res.status(200).send(email)
+            }, 300)
+        }
+        if (key[0] === 'age') {
+            userData.map(function (user) {
+                if (user.age < req.query.age) {
+                    age.push(user)
+                }
+            })
+            setTimeout(function () {
+                res.status(200).send(age)
+            }, 300)
+        }
+        if (key[0] === 'favorites') {
+            userData.map(function (user) {
+                if (user.favorites.includes(req.query.favorites)) {
+                    fav.push(user)
+                }
+            })
+            setTimeout(function () {
+                res.status(200).send(fav)
+            }, 300)
+        }
+    },
 
 
-//3.
-getAdmins: (req, res)=> {
-    res.status(200).send(user.type)
+    //2.
+    getUserById: (req, res) => {
+        if ((userData[req.params.id - 1]) !== undefined) {
+            res.status(200).send(userData[req.params.id - 1])
+        } else {
+            if (userData[req.params.id - 1] == undefined) {
+                res.status(404).json(null)
+            }
+        }
+        //     
+    },
+
+
+    //3.
+    getAdmins: (req, res) => {
+        let admins = []
+        userData.filter(function (user) {
+            if (user.type === "admin") {
+                admins.push(user)
+            }
+        })
+        setTimeout(function () {
+            res.status(200).send(admins)
+        }, 3000)
+    },
+
+
+    //4.
+    getNonAdmins: (req, res) => {
+        let nonadmins = []
+        userData.filter(function (user) {
+            if (user.type !== "admin") {
+                nonadmins.push(user)
+            }
+        })
+        setTimeout(function () {
+            res.status(200).send(nonadmins)
+        }, 3000)
+    },
+
+
+    //5.
+    getUsersByType: (req, res) => {
+        let lotsOfUsers = []
+        userData.filter(function (user) {
+            if (user.type == req.params.usertype) {
+                lotsOfUsers.push(user)
+            }
+        })
+        setTimeout(function () {
+            res.status(200).send(lotsOfUsers)
+        }, 300)
+    },
+
+
+    //6.
+    putUsersId: (req, res) => {
+        let initialUser = userData[req.params.id - 1]
+        let updatedUser = Object.assign(initialUser, req.body)
+        res.status(200).send(userData)
+    },
+
+    //7.
+    postUsers: (req, res) => {
+        let newPersonid = userData.length + 1
+        let newPerson = req.body
+        let newPersonAdded = Object.assign({ id: newPersonid }, newPerson)
+        userData.push(newPersonAdded)
+        res.status(200).send(userData)
+
+    },
+
+    //8.
+    deleteUsers: (req, res) => {
+        let removed = userData.splice(req.params.id - 1, 1);
+        res.status(200).send(removed[0])
+    }
+
 }
-
-
-
-
-
-};
-
-//THIS IS AN EXAMPLE OF WHAT THESE FUNCTIONS SHOULD LOOK LIKE
-// app.get('/auth/me', function (req, res) {
-//     if (!req.user) {
-//       return res.status(200).send("");
-//     }
-//     else {
-      
-//       res.status(200).send(req.user);
-//     }
-//   });
